917c3921313ccf7d45a0cb3d6f6f952ae7181078
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-13 15:23:43 -0500

    refactor: separate navigation bars for logged in users

--- app/routes/_expenses.tsx
+++ app/routes/_expenses.tsx
@@ -1,9 +1,15 @@
 import type { LinksFunction } from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
+import ExpensesHeader from '~/components/navigation/ExpensesHeader'
 import styles from '~/styles/expenses.css'
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 export default function _ExpensesLayout() {
-	return <Outlet />
+	return (
+		<>
+			<ExpensesHeader />
+			<Outlet />
+		</>
+	)
 }
