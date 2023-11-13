20d915fd1dcc94f7ca9d2f9b045773dc41a84c8b
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-13 12:32:54 -0500

    chore: use new AuthForm component

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,17 +1,14 @@
 import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import AuthForm from '~/components/auth/AuthForm'
 import styles from '~/styles/auth.css'
 
 export const meta: MetaFunction = () => [
 	{ title: 'Login' },
-	{ name: 'description', content: 'A central place to track your expenses!' },
+	{ name: 'description', content: 'Log in to track your expenses!' },
 ]
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 export default function Auth() {
-	return (
-		<>
-			<h1>Remix Expenses - User Authentication</h1>
-		</>
-	)
+	return <AuthForm />
 }
