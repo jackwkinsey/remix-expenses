cbb9fbcf4e513c4a63ae1cdb380994c75f42882d
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-13 14:09:32 -0500

    fix: adds marketing styles to home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,11 @@
-import type { MetaFunction } from '@remix-run/node'
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
 import { Link } from '@remix-run/react'
 import { FaArrowRight, FaDollarSign, FaChartBar } from 'react-icons/fa'
+import styles from '~/styles/marketing.css'
 
 export const meta: MetaFunction = () => []
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 export default function Index() {
 	return (
