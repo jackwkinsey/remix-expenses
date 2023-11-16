787aaee1131dc99f64c5553b54dc61bb092e84b3
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-16 16:24:31 -0500

    chore: adds error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,19 @@
 import { cssBundleHref } from '@remix-run/css-bundle'
 import type { LinksFunction, MetaFunction } from '@remix-run/node'
 import {
+	Link,
 	Links,
 	LiveReload,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	isRouteErrorResponse,
+	useRouteError,
 } from '@remix-run/react'
+import type { ReactNode } from 'react'
 import sharedStyles from '~/styles/shared.css'
+import Error from '~/components/util/Error'
 
 export const meta: MetaFunction = () => [
 	{ title: 'Remix Expenses' },
@@ -30,21 +35,72 @@ export const links: LinksFunction = () => [
 	},
 ]
 
-export default function App() {
+type DocumentProps = {
+	title?: string
+	children: ReactNode
+}
+function Document({ title, children }: DocumentProps) {
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				{title && <title>{title}</title>}
 				<Meta />
 				<Links />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta charSet="utf-8" />
 			</head>
 			<body>
-				<Outlet />
+				{children}
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
 			</body>
 		</html>
+	)
+}
+
+export default function App() {
+	return (
+		<Document>
+			<Outlet />
+		</Document>
+	)
+}
+
+export function ErrorBoundary() {
+	const error = useRouteError() as Error
+	console.error('ERROR:', error)
+
+	if (isRouteErrorResponse(error)) {
+		return (
+			<Document title={error.statusText}>
+				<main>
+					<Error title={error.statusText}>
+						<p>
+							{error.data?.message ||
+								'Something went wrong. Please try again later.'}
+						</p>
+						<p>
+							Back to <Link to="/">safety</Link>.
+						</p>
+					</Error>
+				</main>
+			</Document>
+		)
+	}
+
+	return (
+		<Document title="An error occurred!">
+			<main>
+				<Error title="An error occurred!">
+					<p>
+						{error.message || 'Something went wrong. Please try again later.'}
+					</p>
+					<p>
+						Back to <Link to="/">safety</Link>.
+					</p>
+				</Error>
+			</main>
+		</Document>
 	)
 }
