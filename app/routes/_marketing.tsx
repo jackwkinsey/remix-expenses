2a6097472bd58965c912461d39a6cb04b9d9bd39
Jack Kinsey <jack.w.kinsey@gmail.com>
2023-11-17 00:45:33 -0500

    chore: adds caching for marketing pages

--- app/routes/_marketing.tsx
+++ app/routes/_marketing.tsx
@@ -1,8 +1,16 @@
-import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
+import type {
+	HeadersFunction,
+	LinksFunction,
+	LoaderFunctionArgs,
+} from '@remix-run/node'
 import { Outlet } from '@remix-run/react'
 import MainHeader from '~/components/navigation/MainHeader'
 import { getUserFromSession } from '~/data/auth.server'
 import styles from '~/styles/marketing.css'
+
+export const headers: HeadersFunction = () => ({
+	'Cache-Control': 'max-age=3600',
+})
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
